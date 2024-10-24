9f76631fc4c01ae8898e0d059f286c6f93674cf6
410631526 <1qaz2wsx.ud@gmail.com>
2024-10-24 18:35:28 +0800

    1024-2.ts

---
+++ 1024-1.js
@@ -0,0 +1,6 @@
+var age = 25;
+var name = "Alice";
+age = 30;
+name = "Bob";
+console.log(age); // 25
+console.log(name); // Bob
---
+++ 1024-1.ts
@@ -0,0 +1,6 @@
+const age: number = 25;
+let name: string = "Alice";
+age = 30;
+name = "Bob";
+console.log(age); // 25
+console.log(name); // Bob
